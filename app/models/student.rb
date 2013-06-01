89e4031a1ea58863c3f3ff4bb64e8773c31120b3
deepak <deepak@tchowklabs.com>
2013-06-01 10:13:27 +0530

    changes made

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,14 +2,14 @@ class Student < ActiveRecord::Base
    attr_accessible :name, :username, :cls, :section, :email, :mobile, :password
    
    EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-   
-   validates :name, :presence => true, :length=>{:maximum => 25}
-   validates :cls, :presence => true, :length=>{:maximum => 25}
+   pass= /^[0-9]{10,12}$/i
+   validates :name, :presence => true, :length =>{:maximum => 25}
+   validates :cls, :presence => true, :length =>{:maximum => 25}
    validates :section, :presence => true, :length=>{:maximum => 25}
    validates :username, :presence => true, :length=>{:within => 8..25}, :uniqueness => true
    validates :email, :presence => true, :format => EMAIL_REGEX, :confirmation => true
-   validates :mobile, :presence => true
-   validates :password, :presence => true, :length=>{:within => 4..25}
+   validates :mobile, :presence => true, :format => pass, :length =>{:maximum => 10}
+   validates :password, :presence => { :message => "wrong"}, :length=>{:within => 4..25}
      
   
    
